49d0b5b3c1301b193166ae0d2988a98c348e5d53
fullstack290623 <fullstack290623@gmail.com>
2023-12-25 19:07:40 +0200

    .

---
+++ create_country.sql
@@ -0,0 +1,4 @@
+create table countries (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+)
---
+++ insert_users.sql
@@ -0,0 +1,3 @@
+-- 1
+insert into users(username, password, email)
+    VALUES ('itayh', '1234', 'itayhau@gmail.com');
